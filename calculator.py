79a0fee37b6760718d5a45231f75ef8afc6568a3
Nguenang Ameline Raissa <97096086+amelinenar@users.noreply.github.com>
2024-03-05 15:31:26 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+numb1 = int(input("Enter the first numb"))
+numb2 = int(input("Enter the second numb"))
+
+
+operator = input("choose the operator")
+print("1 foraddition")
+print("2 for division")
+result = None
+
+if operator == "1":
+    result = numb1 + numb2 
+if operator == "2":
+    result = numb1 + numb2 
+print(result)
